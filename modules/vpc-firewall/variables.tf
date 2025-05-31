495d0fa90c402ab0d9368280814996962868f187
Jyothi Balaka <JyothiB@MacBook-Pro-3.local>
2025-05-31 14:24:34 -0700

    Remove hardcoded credentials_file for CI/CD compatibility

--- modules/vpc-firewall/variables.tf
+++ modules/vpc-firewall/variables.tf
@@ -2,9 +2,6 @@ variable "project" {}
 variable "region" {
   default = "us-central1"
 }
-variable "credentials_file" {
-  default = "~/gcp/terraform/terraform-key.json"
-}
 variable "allowed_ip_range" {
   description = "IP range allowed for SSH"
   default     = "0.0.0.0/0" # ⚠️ For practice. Replace with your real IP if needed.
